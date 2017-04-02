0e286fd4007165b28e20a893ad434f5380876363
Andrew Nipp <Anipper@Dogsarebombaf.com>
2017-04-01 19:59:10 -0700

    Interfaced the node app with our IIS server.

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/amnipp/', index);
+app.use('/amnipp/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
